775f0e67ade6548f13e8daacf1e2ccbb66ae5999
Panchuk Filipp <fpanchuk@yandex.ru>
2025-11-24 22:15:27 +0300

    feat: added purchases module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import {PurchasesModule} from "../purchases/purchases.module";
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI), PurchasesModule
   ],
   controllers: [AppController],
   providers: [AppService],
